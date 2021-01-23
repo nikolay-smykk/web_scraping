daac0ef2d61a4ba338597bd6143eca1c4177ab22
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2021-01-23 23:33:02 +0300

    like in profile

--- insta/index.js
+++ insta/index.js
@@ -1,5 +1,5 @@
-const puppeteer = require("puppeteer");
-const secret = require("./Secret.js");
+const puppeteer = require('puppeteer');
+const secret = require('./Secret.js');
 
 // const LOGIN = constants.LOGIN;
 // const PASSWORD = constants.PASSWORD;
@@ -8,17 +8,34 @@ const secret = require("./Secret.js");
   const browser = await puppeteer.launch({
     headless: false,
     executablePath:
-      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+      '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
   });
 
   const page = await browser.newPage();
+  await authCall(page);
 
-  await page.goto("http://instagram.com/");
+  //go to akk
+  const USERNAME = 'nikolay_smyk';
+  await page.goto(`http://instagram.com/${USERNAME}`);
+  await page.waitForTimeout(4000);
+  //click image
+
+  await (await page.$('article a')).click();
+  await page.waitForTimeout(4000);
+  await (await page.$$('button'))[3].click();
+
+  //   await browser.close();
+})();
+
+//see story
+
+async function authCall(page) {
+  await page.goto('http://instagram.com/');
   //   await page.waitForTimeout(1000);
-  await page.waitForSelector("input");
+  await page.waitForSelector('input');
 
   //all input on page
-  const inputs = await page.$$("input");
+  const inputs = await page.$$('input');
   await inputs[0].type(secret.LOGIN);
   await inputs[1].type(secret.PASSWORD);
 
@@ -31,46 +48,17 @@ const secret = require("./Secret.js");
 
   //go to page
   await page.waitForTimeout(4000);
-  await page.waitForSelector("button");
-  const SaveButton = (await page.$$("button"))[1];
-  await SaveButton.click();
+  await page.waitForSelector('button');
+  await (await page.$$('button'))[1].click();
+
   //we in page insta
 
   //dont now
   await page.waitForTimeout(4000);
-  const dontNow = (
-    await page.$x("/html/body/div[4]/div/div/div/div[3]/button[2]")
-  )[0];
-  await dontNow.click();
+  await (
+    await page.$x('/html/body/div[4]/div/div/div/div[3]/button[2]')
+  )[0].click();
+
   //dont now
   await page.waitForTimeout(4000);
-  //click Story
-  const buttonStory = (
-    await page.$x(
-      '//*[@id="react-root"]/section/main/section/div/div[1]/div/div/div/div/ul/li[3]/div/button'
-    )
-  )[0];
-  await buttonStory.click();
-
-  await page.waitForTimeout(4000);
-
-  const nextStory = (
-    await page.$x(
-      '//*[@id="react-root"]/section/div[1]/div/section/div/button[2]'
-    )
-  )[0];
-
-  try {
-    while (true) {
-      await page.waitForTimeout(1000);
-      await nextStory.click();
-    }
-  } catch {
-    console.log("all Good and Cancel");
-    await browser.close();
-  }
-
-  //   await browser.close();
-})();
-
-//see story
+}
